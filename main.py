de5e2c9677353a732ea256ab51a8d9fbfe14d913
naveensolanki56912 <naveensolanki56912@gmail.com>
2023-09-20 16:27:49 +0530

    All Files Committed 4

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from tensorflow.keras.models import load_model
 import os
 from deep_learning_pipeline import Model_Pipeline
+import json
 
 model = load_model(os.path.join('model', 'water_problem_classifier.h5'))
 pipeline = Model_Pipeline()
-pipeline.predict(model)
+answer = pipeline.predict(model)
+
+
+
+def get_json():
+    temp = {"result": answer}
+    return temp
+# file_name = 'temp.json'
+#
+# with open(file_name, "w") as json_file:
+#     json.dump(temp, json_file)
+# print('Data Written to Json file.')
+# print(type(os.path.join('temp.json')))
