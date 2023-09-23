326bcc0fe73de2d81680428dcbb1d4c3332118cc
Bhattjatin <bhattjatin03@gmail.com>
2023-09-23 23:33:40 +0530

    jatin second commit

--- app/src/main/java/com/example/jalsanket/MainActivity.kt
+++ app/src/main/java/com/example/jalsanket/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.jalsanket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myxButton = findViewById<Button>(R.id.button2)
+
+
+        myxButton.setOnClickListener {
+            // Define the action you want to perform when the button is clicked
+            val intent = Intent(this, loginoptionsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
